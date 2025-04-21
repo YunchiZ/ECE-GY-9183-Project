ac66e7b33b89ef0345a65a1a375f4cfdf944b31a
Haorong Liang <youjie020726@gmail.com>
2025-04-20 22:53:48 -0400

    Update extract.py

--- data_pipeline/extract.py
+++ data_pipeline/extract.py
@@ -3,16 +3,16 @@ import pandas as pd
 ## change the path to read from local environment
 
 def load_csv():
-    df = pd.read_csv("NewsCategorizer.csv")
+    df = pd.read_csv("/app/raw_data/NewsCategorizer.csv")
     df.columns = df.columns.str.strip()
     return df
 
 def load_csv_welfake():
-    df = pd.read_csv("WELFake_Dataset.csv", encoding='utf-8')
+    df = pd.read_csv("/app/raw_data/WELFake_Dataset.csv", encoding='utf-8')
     df.columns = df.columns.str.strip()
     return df
 
 def load_csv_summary():
-    df = pd.read_csv("summarization.csv", encoding='utf-8')
+    df = pd.read_csv("/app/raw_data/summarization.csv", encoding='utf-8')
     df.columns = df.columns.str.strip()
     return df
