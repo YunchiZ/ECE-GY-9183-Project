cb50778c7c6fe74e76e5a32ba582c6f77028b387
Haorong Liang <youjie020726@gmail.com>
2025-05-10 23:49:06 -0400

    Update mount_block_store.sh

--- mount_block_store.sh
+++ mount_block_store.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-set -e
+set -e  # Exit immediately if any command fails
 
 DEVICE="/dev/vdb1"
-MOUNT_DIR="/mnt/block"
+MOUNT_POINT="/mnt/block"
 
-echo "[INFO] Creating mount directory at $MOUNT_DIR..."
-sudo mkdir -p $MOUNT_DIR
+echo "[INFO] Creating mount point: $MOUNT_POINT"
+sudo mkdir -p $MOUNT_POINT
 
-echo "[INFO] Mounting $DEVICE to $MOUNT_DIR..."
-sudo mount $DEVICE $MOUNT_DIR
+echo "[INFO] Mounting $DEVICE to $MOUNT_POINT"
+sudo mount $DEVICE $MOUNT_POINT
 
-echo "[INFO] Changing ownership to user 'cc'..."
-sudo chown -R cc:cc $MOUNT_DIR
+echo "[INFO] Changing ownership to user 'cc'"
+sudo chown -R cc $MOUNT_POINT
+sudo chgrp -R cc $MOUNT_POINT
 
-echo "[SUCCESS] Block storage mounted to $MOUNT_DIR"
+echo "[SUCCESS] Block volume mounted at $MOUNT_POINT"
