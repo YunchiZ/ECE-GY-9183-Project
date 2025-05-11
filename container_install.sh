a17ae998b55243797a1990f6b60c9c3ac5958c93
YunchiZ <scyyz17@gmail.com>
2025-05-11 16:09:51 -0400

    add contain install

---
+++ container_install.sh
@@ -0,0 +1,17 @@
+# install container toolkit
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/nvidia-container-toolkit.gpg
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+  sed 's#deb https://#deb [signed-by=/etc/apt/keyrings/nvidia-container-toolkit.gpg] https://#' | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+
